7c325b2e19d68a58b7c9ef427e83a146bf3c78d0
Gangaprasad25 <gangaprasad.tv@gmail.com>
2021-01-26 16:44:08 +0530

    added logintest with chrome driver, gitignore

---
+++ src/test/java/com/swagen/testcases/ui/LoginTest.java
@@ -0,0 +1,26 @@
+package com.swagen.testcases.ui;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LoginTest {
+
+	public static void main(String[] args) {
+
+		System.setProperty("webdriver.chrome.driver", "src\\main\\resources\\webdriverbinaries\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.facebook.com/");
+		String expectedtitle = "Facebook – Log in or Sign up";
+		String actualTitle = driver.getTitle();
+		if (expectedtitle.equals(actualTitle)) {
+			System.out.println("Title is as expected");
+
+		} else {
+			System.out.println(String.format("Title is not matching -- expected is : %s but actual is: %s", expectedtitle,actualTitle));
+			
+		}
+		driver.quit();
+
+	}
+
+}
